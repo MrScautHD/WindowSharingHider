881cf3d5e791e04dc0e4cdbb6289d8c9a627514d
MrScautHD <65916181+MrScautHD@users.noreply.github.com>
2026-06-14 17:23:52 +0200

    Implimented usefull functions

--- WindowSharingHider/MainWindow.Designer.cs
+++ WindowSharingHider/MainWindow.Designer.cs
@@ -29,18 +29,87 @@ namespace WindowSharingHider
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.windowListCheckBox = new System.Windows.Forms.CheckedListBox();
+            this.autoHideAllCheckBox = new System.Windows.Forms.CheckBox();
+            this.autoHideWhenVeraCryptOpenCheckBox = new System.Windows.Forms.CheckBox();
+            this.autoHideAppPathTextBox = new System.Windows.Forms.TextBox();
+            this.hideToBackgroundButton = new System.Windows.Forms.Button();
+            this.autostartHiddenCheckBox = new System.Windows.Forms.CheckBox();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
             this.SuspendLayout();
+            // 
+            // autoHideAllCheckBox
+            // 
+            this.autoHideAllCheckBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.autoHideAllCheckBox.Location = new System.Drawing.Point(0, 0);
+            this.autoHideAllCheckBox.Name = "autoHideAllCheckBox";
+            this.autoHideAllCheckBox.Padding = new System.Windows.Forms.Padding(4, 0, 0, 0);
+            this.autoHideAllCheckBox.Size = new System.Drawing.Size(328, 28);
+            this.autoHideAllCheckBox.TabIndex = 0;
+            this.autoHideAllCheckBox.Text = "Auto hide all open windows";
+            this.autoHideAllCheckBox.UseVisualStyleBackColor = true;
+            this.autoHideAllCheckBox.CheckedChanged += new System.EventHandler(this.autoHideAllCheckBox_CheckedChanged);
+            // 
+            // autoHideWhenVeraCryptOpenCheckBox
+            // 
+            this.autoHideWhenVeraCryptOpenCheckBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.autoHideWhenVeraCryptOpenCheckBox.Location = new System.Drawing.Point(0, 28);
+            this.autoHideWhenVeraCryptOpenCheckBox.Name = "autoHideWhenVeraCryptOpenCheckBox";
+            this.autoHideWhenVeraCryptOpenCheckBox.Padding = new System.Windows.Forms.Padding(4, 0, 0, 0);
+            this.autoHideWhenVeraCryptOpenCheckBox.Size = new System.Drawing.Size(328, 28);
+            this.autoHideWhenVeraCryptOpenCheckBox.TabIndex = 1;
+            this.autoHideWhenVeraCryptOpenCheckBox.Text = "Auto hide all when this app is running";
+            this.autoHideWhenVeraCryptOpenCheckBox.UseVisualStyleBackColor = true;
+            this.autoHideWhenVeraCryptOpenCheckBox.CheckedChanged += new System.EventHandler(this.autoHideWhenVeraCryptOpenCheckBox_CheckedChanged);
+            // 
+            // autoHideAppPathTextBox
+            // 
+            this.autoHideAppPathTextBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.autoHideAppPathTextBox.Location = new System.Drawing.Point(0, 56);
+            this.autoHideAppPathTextBox.Name = "autoHideAppPathTextBox";
+            this.autoHideAppPathTextBox.Size = new System.Drawing.Size(328, 20);
+            this.autoHideAppPathTextBox.TabIndex = 2;
+            this.autoHideAppPathTextBox.Text = "C:\\Program Files\\VeraCrypt\\VeraCrypt.exe";
+            // 
+            // hideToBackgroundButton
+            // 
+            this.hideToBackgroundButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.hideToBackgroundButton.Location = new System.Drawing.Point(0, 76);
+            this.hideToBackgroundButton.Name = "hideToBackgroundButton";
+            this.hideToBackgroundButton.Size = new System.Drawing.Size(328, 28);
+            this.hideToBackgroundButton.TabIndex = 3;
+            this.hideToBackgroundButton.Text = "Hide app to background";
+            this.hideToBackgroundButton.UseVisualStyleBackColor = true;
+            this.hideToBackgroundButton.Click += new System.EventHandler(this.hideToBackgroundButton_Click);
+            // 
+            // autostartHiddenCheckBox
+            // 
+            this.autostartHiddenCheckBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.autostartHiddenCheckBox.Location = new System.Drawing.Point(0, 104);
+            this.autostartHiddenCheckBox.Name = "autostartHiddenCheckBox";
+            this.autostartHiddenCheckBox.Padding = new System.Windows.Forms.Padding(4, 0, 0, 0);
+            this.autostartHiddenCheckBox.Size = new System.Drawing.Size(328, 28);
+            this.autostartHiddenCheckBox.TabIndex = 4;
+            this.autostartHiddenCheckBox.Text = "Start with Windows hidden";
+            this.autostartHiddenCheckBox.UseVisualStyleBackColor = true;
+            this.autostartHiddenCheckBox.CheckedChanged += new System.EventHandler(this.autostartHiddenCheckBox_CheckedChanged);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = System.Drawing.SystemIcons.Application;
+            this.notifyIcon.Text = "Window Sharing Hider";
+            this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseClick);
             // 
             // windowListCheckBox
             // 
             this.windowListCheckBox.CheckOnClick = true;
             this.windowListCheckBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.windowListCheckBox.FormattingEnabled = true;
-            this.windowListCheckBox.Location = new System.Drawing.Point(0, 0);
+            this.windowListCheckBox.Location = new System.Drawing.Point(0, 132);
             this.windowListCheckBox.Name = "windowListCheckBox";
-            this.windowListCheckBox.Size = new System.Drawing.Size(328, 384);
-            this.windowListCheckBox.TabIndex = 0;
+            this.windowListCheckBox.Size = new System.Drawing.Size(328, 252);
+            this.windowListCheckBox.TabIndex = 5;
             // 
             // MainWindow
             // 
@@ -48,6 +117,11 @@ namespace WindowSharingHider
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(328, 384);
             this.Controls.Add(this.windowListCheckBox);
+            this.Controls.Add(this.autostartHiddenCheckBox);
+            this.Controls.Add(this.hideToBackgroundButton);
+            this.Controls.Add(this.autoHideAppPathTextBox);
+            this.Controls.Add(this.autoHideWhenVeraCryptOpenCheckBox);
+            this.Controls.Add(this.autoHideAllCheckBox);
             this.Name = "MainWindow";
             this.Text = "Window Sharing Hider";
             this.ResumeLayout(false);
@@ -57,6 +131,12 @@ namespace WindowSharingHider
         #endregion
 
         private System.Windows.Forms.CheckedListBox windowListCheckBox;
+        private System.Windows.Forms.CheckBox autoHideAllCheckBox;
+        private System.Windows.Forms.CheckBox autoHideWhenVeraCryptOpenCheckBox;
+        private System.Windows.Forms.TextBox autoHideAppPathTextBox;
+        private System.Windows.Forms.Button hideToBackgroundButton;
+        private System.Windows.Forms.CheckBox autostartHiddenCheckBox;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
     }
 }
 
--- WindowSharingHider/MainWindow.cs
+++ WindowSharingHider/MainWindow.cs
@@ -1,7 +1,11 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace WindowSharingHider
 {
@@ -17,14 +21,23 @@ namespace WindowSharingHider
                 return Title;
             }
         }
-        public MainWindow()
+        public MainWindow(Boolean startHidden = false)
         {
             InitializeComponent();
+            InitializeAutostartCheckBox();
+            if (startHidden) Shown += (sender, e) => HideToBackground();
             var timer = new Timer();
             timer.Interval = 500;
             timer.Tick += Timer_Tick;
             timer.Start();
         }
+        const String AutostartRegistryPath = @"Software\Microsoft\Windows\CurrentVersion\Run";
+        const String AutostartValueName = "WindowSharingHider";
+        const Int32 VisibleAffinity = 0x0;
+        const Int32 HiddenAffinity = 0x11;
+        readonly Dictionary<IntPtr, Int32> automaticHideOriginalAffinities = new Dictionary<IntPtr, Int32>();
+        Boolean automaticHideActive = false;
+        Boolean initializingControls = false;
         Boolean flagToPreserveSettings = false;
         private void Timer_Tick(object sender, EventArgs e)
         {
@@ -41,18 +54,151 @@ namespace WindowSharingHider
                 else windowListCheckBox.Items.Add(new WindowInfo { Title = window.Value, Handle = window.Key, stillExists = true });
             }
             foreach (var window in windowListCheckBox.Items.Cast<WindowInfo>().ToArray()) if (window.stillExists == false) windowListCheckBox.Items.Remove(window);
+
+            var shouldAutoHideAll = ShouldAutoHideAll();
+            if (!shouldAutoHideAll && automaticHideActive) RestoreAutomaticHideOriginalAffinities();
+            automaticHideActive = shouldAutoHideAll;
+
             foreach (var window in windowListCheckBox.Items.Cast<WindowInfo>().ToArray())
             {
-                var status = WindowHandler.GetWindowDisplayAffinity(window.Handle) > 0;
-                var target = windowListCheckBox.GetItemChecked(windowListCheckBox.Items.IndexOf(window));
-                if (target != status && flagToPreserveSettings)
+                var status = WindowHandler.GetWindowDisplayAffinity(window.Handle);
+                var index = windowListCheckBox.Items.IndexOf(window);
+                if (shouldAutoHideAll)
                 {
-                    WindowHandler.SetWindowDisplayAffinity(window.Handle, target ? 0x11 : 0x0);
-                    status = WindowHandler.GetWindowDisplayAffinity(window.Handle) > 0;
+                    if (!automaticHideOriginalAffinities.ContainsKey(window.Handle)) automaticHideOriginalAffinities[window.Handle] = status;
+                    if (status != HiddenAffinity)
+                    {
+                        WindowHandler.SetWindowDisplayAffinity(window.Handle, HiddenAffinity);
+                        status = WindowHandler.GetWindowDisplayAffinity(window.Handle);
+                    }
                 }
-                windowListCheckBox.SetItemChecked(windowListCheckBox.Items.IndexOf(window), status);
+                else
+                {
+                    var target = windowListCheckBox.GetItemChecked(index) ? HiddenAffinity : VisibleAffinity;
+                    if (target != status && flagToPreserveSettings)
+                    {
+                        WindowHandler.SetWindowDisplayAffinity(window.Handle, target);
+                        status = WindowHandler.GetWindowDisplayAffinity(window.Handle);
+                    }
+                }
+                windowListCheckBox.SetItemChecked(index, status > 0);
             }
             flagToPreserveSettings = true;
+        }
+
+        private void autoHideAllCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            Timer_Tick(sender, e);
+        }
+
+        private void autoHideWhenVeraCryptOpenCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            Timer_Tick(sender, e);
+        }
+
+        private void hideToBackgroundButton_Click(object sender, EventArgs e)
+        {
+            HideToBackground();
+        }
+
+        private void autostartHiddenCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (initializingControls) return;
+
+            SetAutostartHidden(autostartHiddenCheckBox.Checked);
+        }
+
+        private void notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) RestoreFromBackground();
+        }
+
+        private Boolean ShouldAutoHideAll()
+        {
+            return autoHideAllCheckBox.Checked || (autoHideWhenVeraCryptOpenCheckBox.Checked && IsConfiguredAppRunning());
+        }
+
+        private Boolean IsConfiguredAppRunning()
+        {
+            var configuredPath = autoHideAppPathTextBox.Text.Trim().Trim('"');
+            if (String.IsNullOrWhiteSpace(configuredPath)) return false;
+
+            foreach (var process in Process.GetProcesses())
+            {
+                try
+                {
+                    var processPath = process.MainModule?.FileName;
+                    if (String.Equals(processPath, configuredPath, StringComparison.OrdinalIgnoreCase)) return true;
+                }
+                catch
+                {
+                    if (String.Equals(process.ProcessName, Path.GetFileNameWithoutExtension(configuredPath), StringComparison.OrdinalIgnoreCase)) return true;
+                }
+                finally
+                {
+                    process.Dispose();
+                }
+            }
+            return false;
+        }
+
+        private void RestoreAutomaticHideOriginalAffinities()
+        {
+            foreach (var window in windowListCheckBox.Items.Cast<WindowInfo>().ToArray())
+            {
+                if (!automaticHideOriginalAffinities.TryGetValue(window.Handle, out Int32 originalAffinity)) continue;
+
+                var index = windowListCheckBox.Items.IndexOf(window);
+                WindowHandler.SetWindowDisplayAffinity(window.Handle, originalAffinity);
+                windowListCheckBox.SetItemChecked(index, originalAffinity > 0);
+            }
+            automaticHideOriginalAffinities.Clear();
+        }
+
+        private void HideToBackground()
+        {
+            notifyIcon.Visible = true;
+            ShowInTaskbar = false;
+            Hide();
+        }
+
+        private void RestoreFromBackground()
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+            ShowInTaskbar = true;
+            Activate();
+            notifyIcon.Visible = false;
+        }
+
+        private void InitializeAutostartCheckBox()
+        {
+            initializingControls = true;
+            autostartHiddenCheckBox.Checked = IsAutostartHiddenEnabled();
+            initializingControls = false;
+        }
+
+        private Boolean IsAutostartHiddenEnabled()
+        {
+            using (var key = Registry.CurrentUser.OpenSubKey(AutostartRegistryPath, false))
+            {
+                var value = key?.GetValue(AutostartValueName) as String;
+                return String.Equals(value, GetAutostartCommand(), StringComparison.OrdinalIgnoreCase);
+            }
+        }
+
+        private void SetAutostartHidden(Boolean enabled)
+        {
+            using (var key = Registry.CurrentUser.CreateSubKey(AutostartRegistryPath))
+            {
+                if (enabled) key?.SetValue(AutostartValueName, GetAutostartCommand());
+                else key?.DeleteValue(AutostartValueName, false);
+            }
+        }
+
+        private static String GetAutostartCommand()
+        {
+            return "\"" + Application.ExecutablePath + "\" --hidden";
         }
     }
 }
